de74b9ef447c1b3a787906e8f5ace581fed1c285
Alejandro Di Stefano <addistefano@live.com.ar>
2025-11-03 11:57:44 -0300

    Clase 04

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,247 +1,191 @@
-// Funciones
+// Unidad 4: Arrays y Objetos
 
-// const numeroA = 1;
-// const numeroB = 2;
+// const miArrayDeNumeros = [1, 2, 3, 5, 12];
+//              Indices= [0, 1, 2, 3, 4]; -> Indices de los elementos del Array
 
-// const resultado = numeroA + numeroB;
+// miArrayDeNumeros = 2;
+// console.log(miArrayDeNumeros)
+// console.log(typeof miArrayDeNumeros)
+// Los Arrays tienen sus propios metodos (funciones)
+// miArrayDeNumeros.push(5); // Agrega al final del Array
+// miArrayDeNumeros.push(545);
+// miArrayDeNumeros.push(45);
 
-// console.log("Felicitaciones el resultado es: "+resultado);
+// miArrayDeNumeros.pop(); // Elimina el ultimo elemento (con el indice mas alto)
 
-// const numeroC = 10;
-// const numeroD = 25;
 
-// const resultado1 = numeroC + numeroD;
+// Estos metodos modifican todos los indices del Array
+// miArrayDeNumeros.shift(); // Elimina el primer elemento (con el indice 0) -> Destructivo
+// miArrayDeNumeros.unshift(3); // Agrega al principio del Array -> Destructivo
+// Estos metodos modifican todos los indices del Array
 
-// console.log("Felicitaciones el resultado es: "+resultado1);
 
-// const numeroE = 100;
-// const numeroF = 205;
+// console.log(miArrayDeNumeros)
 
-// const resultado2 = numeroE + numeroF;
+// console.log(miArrayDeNumeros.indexOf(0))
 
-// console.log("Felicitaciones el resultado es: "+resultado2);
 
-// Declaramos la funcion
-// Declara la variable GLOBAL resultado
-// let resultado = 0;
+// const miArrayDeNumeros = [1, 2, 3, 5, 12, 5, -8, 15];
+// console.log(miArrayDeNumeros)
+// console.log(miArrayDeNumeros.length) // Nos muestra la cantida de valores en numero entero que tiene nuestro Array
 
-// function sumarDosNumerosProporcionadosPorElUsuario() {
-//     const numeroA = parseFloat(prompt("Ingrese un numero A"));
-//     const numeroB = parseFloat(prompt("Ingrese un numero B"));
-//     //const resultado = numeroA + numeroB; // En este momento la variable/constante resultado es local
-//     resultado = numeroA + numeroB; // Reasignacion dentro de la funcion
-//     console.log("Felicitaciones el resultado es: "+resultado);
+
+// // console.log(miArrayDeNumeros[4])
+// const cantidad = miArrayDeNumeros.length;
+
+// for(let i = 0; i < cantidad; i++){
+//     console.log("Valor: " + miArrayDeNumeros[i])
 // }
 
-// // Invocamos a la funcion las veces que se necesite
-// sumarDosNumerosProporcionadosPorElUsuario(); 
-// console.log("Felicitaciones el resultado de sumarDosNumerosProporcionadosPorElUsuario es: "+resultado);
-
-// alert("Vamos de nuevo");
-
-// sumarDosNumerosProporcionadosPorElUsuario(); 
-// console.log("Felicitaciones el resultado de sumarDosNumerosProporcionadosPorElUsuario es: "+resultado);
+// console.log(miArrayDeNumeros.join(" + "));
+// console.log(miArrayDeNumeros.join(" | "));
+// console.log(miArrayDeNumeros.join(" / "));
+// console.log(miArrayDeNumeros.join(" "));
+// console.log(miArrayDeNumeros.join(""));
 
 
-// Declaramos las variables GLOBALES
-// let resultado = 0;
-// let numeroA = parseFloat(prompt("Ingrese un numero A"));
-// let numeroB = parseFloat(prompt("Ingrese un numero B"));
+// console.log(miArrayDeNumeros.indexOf(0)); // Retorna un numero entero -> Indice del Elemento -> Solo muestra el 1ro que encuentra
+// console.log(miArrayDeNumeros.includes(0)); // Retorna un Boleano si existe o no el elemento
 
-// function sumarDosNumerosProporcionadosPorElUsuario(){
-//     resultado = numeroA + numeroB;
-//     console.log("Felicitaciones el resultado es: "+resultado);
+
+// const miArrayDeStrings = ["Hola", "Mundo", "CoderHouse"];
+// console.log(miArrayDeStrings.join(" - "))
+
+// for(let i = 0; i < miArrayDeStrings.length; i++){
+//     console.log("En el indice : " + i + " esta el valor " + miArrayDeStrings[i]);
 // }
-// function restarDosNumerosProporcionadosPorElUsuario(){
-//     resultado = numeroA - numeroB;
-//     console.log("Felicitaciones el resultado es: "+resultado);
-// }
-// function multiplicarDosNumerosProporcionadosPorElUsuario(){
-//     resultado = numeroA * numeroB;
-//     console.log("Felicitaciones el resultado es: "+resultado);
-// }
-// function dividirDosNumerosProporcionadosPorElUsuario(){
-//     resultado = numeroA / numeroB;
-//     if(numeroB !== 0) {
-//         console.log("Felicitaciones el resultado es: "+resultado);
-//     } else{
-//         alert("No se puede dividir por 0")
+
+// console.log(miArrayDeStrings.includes("CoderHouse"));
+
+// console.log(miArrayDeStrings.sort()); // Ordena Alfabeticamente -> Cambia los indices del Array
+// console.log(miArrayDeStrings.sort().reverse()); // Ordena Alfabeticamente Invertido -> Cambia los indices del Array
+
+// Objetos
+
+// const producto = {
+//     id: 1,
+//     nombre: "Azucar",
+//     precio: 1200.50,
+//     stock: 20
+// };
+// console.log(typeof producto);
+// console.log(producto);
+
+// const productos = [
+//     {
+//         id: 1,
+//         nombre: "Azucar",
+//         precio: 1200.50,
+//         stock: 20
+//     },
+//     {
+//         id: 2,
+//         nombre: "Yerba",
+//         precio: 2200.50,
+//         stock: 200
+//     },
+//     {
+//         id: 3,
+//         nombre: "Gaseosa",
+//         precio: 1800.50,
+//         stock: 500
+//     },
+//     {
+//         id: 4,
+//         nombre: "Harina",
+//         precio: 990.50,
+//         stock: 202
 //     }
-// }
-// sumarDosNumerosProporcionadosPorElUsuario();
-// restarDosNumerosProporcionadosPorElUsuario();
-// multiplicarDosNumerosProporcionadosPorElUsuario();
-// dividirDosNumerosProporcionadosPorElUsuario();
-// sumarDosNumerosProporcionadosPorElUsuario();
-// sumarDosNumerosProporcionadosPorElUsuario();
-// sumarDosNumerosProporcionadosPorElUsuario();
-// sumarDosNumerosProporcionadosPorElUsuario();
+// ];
 
+// console.log(productos[0]);
+// console.log(productos[2].id);
+// console.log(productos[2].nombre);
+// console.log(productos[2].precio);
+// console.log(productos[2].stock);
 
-// Funciones usando Parametros
-// Declaramos las variables GLOBALES
-// let resultado = 0;
-// let numeroA = parseFloat(prompt("Ingrese un numero A"));
-// let numeroB = parseFloat(prompt("Ingrese un numero B"));
+// console.log(productos);
 
-// function sumar(a, b) {
-//     resultado = a + b;
-//     console.log("Felicitaciones el resultado es: " + resultado);
-// }
-// function restar(a, b) {
-//     resultado = a - b;
-//     console.log("Felicitaciones el resultado es: " + resultado);
-// }
-// function multiplicar(a, b) {
-//     resultado = a * b;
-//     console.log("Felicitaciones el resultado es: " + resultado);
-// }
-// function dividir(a, b) {
-//     resultado = a / b;
-//     if (b !== 0) {
-//         console.log("Felicitaciones el resultado es: " + resultado);
-//     } else {
-//         alert("No se puede dividir por 0")
+// productos.push(
+//     {
+//         id: 5,
+//         nombre: "Chocolate",
+//         precio: 6990.50,
+//         stock: 10     
 //     }
+// );
+
+// console.log(productos);
+
+// const productoImportado = {
+//         id: 6,
+//         nombre: "Chocolate Italiano",
+//         precio: 12990.50,
+//         stock: 100,
+//         origen: "Italia",
+//         codigo: "asdasdas"
 // }
 
-// // Al momento de la invocacion pasar los argumentos
-// sumar(numeroA, numeroB);
-// restar(numeroA, numeroB);
-// multiplicar(numeroA, numeroB);
-// dividir(numeroA, numeroB);
-
-
-// Intentemos crear una Calculadora
-
-// let numeroA = parseFloat(prompt("Ingrese un numero A"));
-// let numeroB = parseFloat(prompt("Ingrese un numero B"));
-// let operacion = prompt("Ingrese la operacion");
-
-// function calcular(a, b, op) {
-//     const msgError = "No se puede dividir por cero";
-//     let resultado = 0;
-//     switch (op) {
-//         case "+":
-//             resultado = a + b;
-//             console.log("Felicitaciones el resultado de la suma es: " + resultado);
-//             break;
-//         case "-":
-//             resultado = a - b;
-//             console.log("Felicitaciones el resultado de la resta es: " + resultado);
-//             break;
-//         case "*":
-//             resultado = a * b;
-//             console.log("Felicitaciones el resultado de la multiplicacion es: " + resultado);
-//             break;
-//         case "/":
-//             resultado = a / b;
-//             if (b !== 0) {
-//                 console.log("Felicitaciones el resultado de la division es: " + resultado);
-//             } else {
-//                 alert(msgError);
-//                 console.error(msgError);
-//             }
-//             break;
-//         default:
-//             console.error("Error, Ingrese una Operacion valida");
+// productos.push(productoImportado);
+// console.log(productos);
+// productos.push(
+//     {
+//         id: 7,
+//         nombre: "Chocolate Suizo",
+//         precio: 12990.50,
+//         stock: 100,
+//         origen: "Suecia",
+//         codigo: "CHS123"
 //     }
-// }
+// )
+// console.log(productos);
 
-// calcular(numeroA, numeroB, operacion);
+// const carrito = [];
 
+// // carrito.push(productos);
 
-// Funciones Anonimas (Siempre guarden las funciones en const)
+// carrito.push(productos[0], productos[2], productos[3]);
+// console.log(carrito);
 
-// const saludar = function(nombre){
-//     return "Hola, " + nombre + ", como estas?"; // Retorna un String
-// }
+// Cuando quiero darle un formato Unico al Producto
 
-// const sumar = function(a,b){
-//     return a + b; // Evito generar una nueva variable llamada resultado por ejemplo
-// }
+// Clases
 
-// console.log(saludar("Alejandro"))
-// console.log(saludar("Blas"))
-// console.log(sumar(12,55));
+class Producto {
 
-// // saludar = "Hola";
+    constructor(id, nombre, precio, stock, origen, codigo){
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+        this.origen = origen;
+        this.codigo = codigo;
+    }
 
-// console.log(saludar("Mariana"))
+    sumarIVA() {
+        this.precio = parseFloat((this.precio * 1.21).toFixed(2));
+    }
+}
 
-// Las funciones flecha tienen un return implicito, solo cuando se pasa
-// un parametro y la funcion en si no es compleja
+const productosUsandoClase = [];
 
-// Ejemplo:
-// const saludar = (nombre) => "Hola, " + nombre;
-// console.log(saludar("Alejandro"))
-// const sumarOConcatenar = (a,b) => a + b;
-// console.log(sumarOConcatenar("Hola", " mundo"));
-// console.log(sumarOConcatenar(10, 55));
+// Utilizamos la Clase Producto haciendo una Instancia para poder usar su Contructor
+productosUsandoClase.push(new Producto(1, "Azucar", 1900, 20, "Argentina", "AZ001"));
+productosUsandoClase.push(new Producto(2, "Yerba", 1650, 120, "Argentina", "YR001"));
+productosUsandoClase.push(new Producto(3, "Chocolate", 9650, 150));
 
-// Intentemos crear una Calculadora usando funcion flecha
+// productosUsandoClase.sumarIVA() // El metodo se aplica a cada elemento
 
-// let numeroA = parseFloat(prompt("Ingrese un numero A"));
-// let numeroB = parseFloat(prompt("Ingrese un numero B"));
-// let operacion = prompt("Ingrese la operacion");
+const productoX = new Producto(4, "Cocolate Italiano", 12990, 12, "Italia", "CHI001");
 
-// const calcular = (a, b, op) =>{
-//     switch(op){
-//         case "+":
-//             return a + b;
-//         case "-":
-//             return a - b;
-//         case "*":
-//             return a * b;
-//         case "/":
-//             if(b === 0){
-//                 return "Error, no se puede dividir por cero";
-//             } else {
-//                 return a / b;
-//             }
-//         default:
-//             return "Error, operacion invalida";
-//     }
-// }
+productosUsandoClase.push(productoX);
 
-// console.log(calcular(numeroA, numeroB, operacion));
+for(const producto of productosUsandoClase){
+    producto.sumarIVA();
+    console.log(`El producto con ID: ${producto.id} es: 
+        ${producto.nombre}. - El precio de ${producto.nombre} 
+        es $${producto.precio}.- y existen en Stock: ${producto.stock} 
+        Unidades. Origen: ${producto.origen}`);
+}
 
-
-// Hacemos lo mismo pero usando un do while
-
-
-// const calcular = (a, b, op) => {
-//     switch (op) {
-//         case "+":
-//             return a + b;
-//         case "-":
-//             return a - b;
-//         case "*":
-//             return a * b;
-//         case "/":
-//             if (b === 0) {
-//                 return "Error, no se puede dividir por cero";
-//             } else {
-//                 return a / b;
-//             }
-//         default:
-//             return "Error, operacion invalida";
-//     }
-// }
-// let continuar;
-
-// do {
-//     let numeroA = parseFloat(prompt("Ingrese un numero A"));
-//     let numeroB = parseFloat(prompt("Ingrese un numero B"));
-//     let operacion = prompt("Ingrese la operacion");
-
-//     alert(calcular(numeroA, numeroB, operacion));
-
-//     continuar = prompt("¿Desea continuar? (si/no)").toLowerCase();
-// } while (continuar === 'si')
-
-// alert("Gracias por utilizar la Calculadora");
-
-
-
-
+console.table(productosUsandoClase);
